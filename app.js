a2b26a82f347a9b1e3d05fed8bfce7165bcec33f
Maksym Polinovskyi <mmykesser@gmail.com>
2025-01-22 21:12:17 +0100

    Homework #22 completed

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-console.log("Hello Node.js");
+const express = require('express');
+const app = express();
 
-const fs = require('fs');
+app.use(express.json());
 
-fs.readFile('data.txt', 'utf8', (err, data) => {
-	if(err){
-		console.error("Error reading a file:", err);
-		return;
-	}
-	console.log("Contents of the file data.txt:", data);
-}); 
+app.use((req,res,next)=>{
+    console.log(`Method: ${req.method}, URL: ${req.url}`);
+    next();
+});
+
+app.get('/', (req, res) =>{
+    res.send('Hello from Express.js!');
+})
+
+
+app.get('/users/:id', (req, res)=>{
+    const userID = req.userId = req.params.id;
+    res.send(`User ID: ${userID}`);
+})
+
+app.post('/submit', (req, res) =>{
+    const userData = req.body;
+    res.send(`Data received: Name: ${userData.name}, Email: ${userData.email}`);
+})
+
+app.use((req,res)=>{
+    res.status(404).send('Page not Found');
+})
+
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+})
